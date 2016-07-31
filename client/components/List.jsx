475e047a7a6bc26b54c40c3f4e60c3e18f0a9a60
David Ko <david.ko@damncarousel.com>
2016-07-31 15:59:42 -0400

    Style cards and lists

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 require('../stylesheets/list.scss');
+require('../stylesheets/cards.scss');
 
 class List extends React.Component {
   render() {
     const colStyle = {
-      left: `${this.props.index * 280 + (20 * this.props.index)}px`
+      left: `${this.props.index * 280 + (40 * this.props.index)}px`
     }
 
     return (
@@ -14,16 +15,18 @@ class List extends React.Component {
         className="list"
         style={colStyle}
       >
-        <p>{this.props.name}</p>
-        <ul>
-          {this.props.cards.map((card, idx) => <li key={`list-card-${idx}`}>{card.name}</li>)}
+        <p className="list__center-container list__name">{this.props.name}</p>
+        <ul className="cards">
+          {this.props.cards.map((card, idx) => <li className="cards__card" key={`list-card-${idx}`}>{card.name}</li>)}
         </ul>
-        <button 
-          className="btn btn-default"
-          onClick={this.props.openAddCardModal}
-        >
-          Add card...
-        </button>
+        <div className="list__center-container">
+          <button 
+            className="btn btn-default list__add-card"
+            onClick={this.props.openAddCardModal}
+          >
+            Add card...
+          </button>
+        </div>
       </li>
     );
   }
